36dba417b8a40608c3c3dc47bba8f2dbb694f45e
RoushanWebpristine <roushanwebpristine@gmail.com>
2025-08-01 23:32:51 +0530

    update api

--- api/users.js
+++ api/users.js
@@ -1,10 +1,44 @@
-const connectDB = require('../utils/db');
+const mongoose = require('mongoose');
 const User = require('../models/User');
 
-module.exports = async (req, res) => {
-  await connectDB();
+let isConnected = false;
 
-  if (req.method === 'GET') {
+// Reuse the connection on hot reloads
+async function connectToDB() {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  isConnected = true;
+}
+
+// API handler
+module.exports = async (req, res) => {
+  await connectToDB();
+
+  if (req.method === 'POST') {
+    try {
+      const { firstname, lastname, email, mobile, description } = req.body;
+
+      // Check for existing email or mobile
+      const existing = await User.findOne({
+        $or: [{ email }, { mobile }]
+      });
+
+      if (existing) {
+        return res.status(409).json({ message: "User with this email or mobile already exists." });
+      }
+
+      const newUser = new User({ firstname, lastname, email, mobile, description });
+      await newUser.save();
+      res.status(201).json(newUser);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  else if (req.method === 'GET') {
     try {
       const users = await User.find();
       res.status(200).json(users);
@@ -13,29 +47,7 @@ module.exports = async (req, res) => {
     }
   }
 
-  else if (req.method === 'POST') {
-    const { firstname, lastname, email, mobile, description } = req.body;
-
-    try {
-      const existingUser = await User.findOne({
-        $or: [{ email }, { mobile }]
-      });
-
-      if (existingUser) {
-        return res.status(409).json({
-          error: `User already exists with this ${existingUser.email === email ? 'email' : 'mobile'}`
-        });
-      }
-
-      const newUser = new User({ firstname, lastname, email, mobile, description });
-      await newUser.save();
-      res.status(201).json(newUser);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  }
-
   else {
-    res.status(405).json({ error: 'Method not allowed' });
+    res.status(405).json({ message: 'Method not allowed' });
   }
 };
