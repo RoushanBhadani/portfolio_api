6b7dd41660d3631f44c44d06f04f1e6896af332e
RoushanWebpristine <roushanwebpristine@gmail.com>
2025-08-02 02:12:17 +0530

    update api

--- api/users.js
+++ api/users.js
@@ -2,19 +2,24 @@ import connectDB from '../utils/db.js';
 import User from '../models/User.js';
 
 export default async function handler(req, res) {
-  await connectDB();
+  try {
+    await connectDB();
 
-  if (req.method === 'GET') {
-    const users = await User.find();
-    return res.status(200).json(users);
+    if (req.method === 'GET') {
+      const users = await User.find();
+      return res.status(200).json(users);
+    }
+
+    if (req.method === 'POST') {
+      const { firstname, lastname, mobile, email, description } = req.body;
+      const newUser = new User({ firstname, lastname, mobile, email, description });
+      await newUser.save();
+      return res.status(201).json(newUser);
+    }
+
+    return res.status(405).json({ message: 'Method not allowed' });
+  } catch (error) {
+    console.error('API Error:', error);
+    return res.status(500).json({ message: 'Internal Server Error', error: error.message });
   }
-
-  if (req.method === 'POST') {
-    const { firstname, lastname, mobile, email, description } = req.body;
-    const newUser = new User({ firstname, lastname, mobile, email, description });
-    await newUser.save();
-    return res.status(201).json(newUser);
-  }
-
-  return res.status(405).json({ message: 'Method not allowed' });
 }
