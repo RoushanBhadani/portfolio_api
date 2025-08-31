5057fe3b3ba2e96f7bb58d9b1273302d42fd621b
RoushanWebpristine <roushanwebpristine@gmail.com>
2025-08-31 13:43:43 +0530

    Revert "implement subscriber"
    This reverts commit 921320a5aab6d5ebe9fbd20a839d0ebf70509536.

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,6 @@ const router = express.Router();
 
 connectDB();
 
-router.use(cors());
-router.use(express.json());
-
 router.get('/', async (req, res) => {
   const portfolio = await Portfolio.find();
   res.status(200).json(portfolio);
